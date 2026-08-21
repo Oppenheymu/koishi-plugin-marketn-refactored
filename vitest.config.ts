11931196f8fdc41b7165c47e3f6eb5596a9b67e0
oppenheymu <oppenheymu@gmail.com>
2026-08-21 17:10:15 +0800

    test: 启用 Vue 组件测试转换

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,8 @@
+import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+    plugins: [vue()],
     test: {
         include: ['src/**/*.test.ts', 'client/**/*.test.ts'],
         environment: 'node',
